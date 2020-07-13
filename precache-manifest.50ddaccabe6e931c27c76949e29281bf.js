961c2cf2b6be4d3adb470479fea5f07d1a8c3572
gyanendraknojiya <gyanendrak064@gmail.com>
2020-07-14 02:46:55 +0530

    temp build complete

---
+++ precache-manifest.50ddaccabe6e931c27c76949e29281bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26a84af2956918636e1051bbf7106b94",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ffe11d121b3fb2175ac8",
+    "url": "./static/css/main.0f3b4605.chunk.css"
+  },
+  {
+    "revision": "803c65e5154cf2f0bfd2",
+    "url": "./static/js/2.863dd508.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.863dd508.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffe11d121b3fb2175ac8",
+    "url": "./static/js/main.0eabfafb.chunk.js"
+  },
+  {
+    "revision": "3a783b18d9d7eec6d823",
+    "url": "./static/js/runtime-main.791bfbe9.js"
+  }
+]);
